f6b27482fa9d96c585745db7572f458d57edd15d
Jenna Leopold <jleopold424@gmail.com>
2022-04-12 11:23:54 -0400

    completed lesson

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,7 +1,66 @@
 const { Comment, User } = require('../models')
 
-// Work Here
-// Work Here
+const GetAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.findAll()
+    res.send(comments)
+  } catch (error) {
+    throw error
+  }
+}
 
-// Dont forget to export your functions
-module.exports = {}
+const GetCommentDetails = async (req, res) => {
+  try {
+    const comment = await Comment.findByPk(req.params.comment_id)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeleteComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    await Comment.destroy({ where: { id: commentId } })
+    res.send({ message: `Deleted comment with an id of ${commentId}` })
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateComment = async (req, res) => {
+  try {
+    let commentId = parseInt(req.params.comment_id)
+    let updatedComment = await Comment.update(req.body, {
+      where: { id: commentId },
+      returning: true
+    })
+    res.send(updatedComment)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreateComment = async (req, res) => {
+  try {
+    let ownerId = parseInt(req.params.user_id)
+    let twertId = parseInt(req.params.twert_id)
+    let commentBody = {
+      ownerId,
+      twertId,
+      ...req.body
+    }
+    let comment = await Comment.create(commentBody)
+    res.send(comment)
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetAllComments,
+  GetCommentDetails,
+  DeleteComment,
+  UpdateComment,
+  CreateComment
+}
--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -1,6 +1,13 @@
 const Router = require('express').Router()
-
-// You Do
-// Implement Crud Operations For Comments
-// You Do
+const controller = require('../controllers/CommentController')
+// http://localhost:3001/api/comments/all
+Router.get('/all', controller.GetAllComments)
+// http://localhost:3001/api/comments/view/1
+Router.get('/view/:comment_id', controller.GetCommentDetails)
+// http://localhost:3001/api/comments/1
+Router.delete('/:comment_id', controller.DeleteComment)
+// http://localhost:3001/api/comments/2
+Router.put('/:comment_id', controller.UpdateComment)
+// http://localhost:3001/api/comments/1/3
+Router.post('/:user_id/:twert_id', controller.CreateComment)
 module.exports = Router
